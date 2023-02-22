045a976eab5944e65a13e94e46c6d7022e04c076
Justin Kim <justinkim2015@gmail.com>
2023-02-22 12:41:08 +0900

    Add hashset

--- HashSets.java
+++ HashSets.java
@@ -1,5 +1,14 @@
+import java.util.HashSet;
+
 public class HashSets {
   public static void main(String[] args) {
-    
+    HashSet<String> cars = new HashSet<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("BMW");
+    cars.add("Mazda");
+    System.out.println(cars);
   }
 }
+
